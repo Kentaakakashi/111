32241c255e1007c2c0bfa6a6385fd29010b81069
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-05-23 17:31:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -230,6 +230,8 @@ app.get("/", (req, res) => {
   res.send("Trust Issues backend running")
 })
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000")
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log("Server running")
 })
