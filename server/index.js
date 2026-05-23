735a2e55c1e3d8cdbbcfa6a5d3fb14db3649f53c
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-05-23 17:39:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -232,6 +232,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => {
-  console.log("Server running")
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`)
 })
